59bdf147a5d53181f8c8fb11da71ce228196d291
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2024-12-25 20:36:39 +0100

    set up node.js

---
+++ server/server.js
@@ -0,0 +1,37 @@
+const express = require('express'); // framework for webservers and the creation of api-endpoints
+const mysql = require('mysql'); // connection to mysql db
+const cors = require('cors'); // middleware for cross-origin requests
+require('dotenv').config({override : true}); //.env file
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('mysql error: ', err);
+        return;
+    }
+    console.log('mysql connected');
+});
+
+app.get('/api/user', (req, res) => {
+
+    db.query('SELECT * FROM user', (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server runs on port ${PORT}`));
